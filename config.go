945c5ac7db69fcf504f47ef04e47e65b018c62e1
Alan <zzg@daozzg.com>
2019-09-10 17:08:11 +0800

    Update config.go

--- config.go
+++ config.go
@@ -56,10 +56,6 @@ func newCors(config Config) *cors {
 
 func (cors *cors) applyCors(c *gin.Context) {
 	origin := c.Request.Header.Get("Origin")
-	if len(origin) == 0 {
-		// request is not a CORS request
-		return
-	}
 	host := c.Request.Header.Get("Host")
 
 	if origin == "http://"+host || origin == "https://"+host {
